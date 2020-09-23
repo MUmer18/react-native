2e0724c6af275e3cac95783f89e6cc40e78adfd2
MUmer04 <umer_1309e@aptechgdn.com>
2020-09-23 21:42:55 +0500

    login page

--- react_workspace/hello_world/App.js
+++ react_workspace/hello_world/App.js
@@ -13,6 +13,8 @@ import {
   ScrollView,
   View,
   Text,
+  TextInput,
+  Button,
   StatusBar,
 } from 'react-native';
 
@@ -27,14 +29,44 @@ import {
 const App: () => React$Node = () => {
   return (
     <View style={{
-      backgroundColor:"blue",
       flex:1,
-      alignItems:"center"
+      alignItems:"center",
+      justifyContent:"center",
     }}>
       <Text style={{
-        fontSize:20,
-        color:"#fff"
-      }}>Hello world</Text>
+        fontSize:30
+      }}>Login</Text>
+      <Text style={{fontSize:15}}>Please signin to continue</Text>
+      <View style={{width:"80%",margin:20,}}></View>
+     <TextInput placeholder="Email" style={{
+        backgroundColor:"#eaeaea",
+        borderWidth: 0,
+        padding: 5,
+        width: 300,
+    
+      }} />
+      <TextInput placeholder="password" placeholderTextColor="000" style={{
+        backgroundColor:"#eaeaea",
+        borderWidth: 0,
+        padding: 5,
+        width: 300,
+        marginTop:20}}/>
+
+        <View style={{
+          marginTop:20,        
+  
+            width: 300,
+            marginTop:20
+          }}> 
+        <Button title="Login" color="#ff0000" onPress={() =>{
+          alert("Button Passed")
+        }}
+        
+        
+        />
+        </View>
+
+
     </View>
   );
 };
